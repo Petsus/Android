b13bce3f2568124f97c6071d64b02e747647bfc6
Yuki Makoto <gluanfernandes13@gmail.com>
2022-09-04 01:06:38 -0300

    First commit

---
+++ app/src/main/java/br/com/petsus/AppApplication.kt
@@ -0,0 +1,9 @@
+package br.com.petsus
+
+import android.app.Application
+
+class AppApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/br/com/petsus/api/manager/RequestAPI.kt
@@ -0,0 +1,7 @@
+package br.com.petsus.api.manager
+
+object RequestAPI {
+
+    
+
+}
